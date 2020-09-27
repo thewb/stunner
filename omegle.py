a1224836eac7fd506fba5624e7a1db01fd0c1704
wb <wb@Macintosh.local>
2020-09-27 08:19:56 -0700

    modifed omegle server status script

--- omegle.py
+++ omegle.py
@@ -2,18 +2,9 @@
 
 import requests
 import json
-import socket
-import geoip
 
-from geolite2 import geolite2
 S = requests.Session()
-reader = geolite2.reader()
 
 resp = S.get("http://chatserv.omegle.com/status")
-
-for i in resp.json()['servers']:
-	ip = socket.gethostbyname(i + '.omegle.com')
-	r = S.get("http://" + i + ".omegle.com/status")
-	j = r.json()
-	r = reader.get(ip)
-	print('Server: {} count: {}'.format(i, j["count"]))
+j = resp.json()
+print(json.dumps(j, indent=4))
