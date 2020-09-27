8c092a482d33b9a6cacec00255d26a6e088b06c3
wb <wb@Macintosh.local>
2020-09-27 08:24:02 -0700

    modifed omegle server status script

--- omegle.py
+++ omegle.py
@@ -2,8 +2,11 @@
 
 import requests
 import json
+import sys
 
 S = requests.Session()
+
+server = sys.argv[1] if not sys.argv[1] else "chatserv"
 
 resp = S.get("http://chatserv.omegle.com/status")
 j = resp.json()
